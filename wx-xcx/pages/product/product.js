ee00a7bffd5e61f804ee14f727073128656d6b70
fenglulu355 <732038907@qq.com>
2020-05-15 18:01:26 +0800

    wx

--- wx-xcx/pages/product/product.js
+++ wx-xcx/pages/product/product.js
@@ -8,30 +8,55 @@ Page({
     proli: [{
       img: '/image/pro01.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 0
     }, {
       img: '/image/pro02.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 1
     }, {
       img: '/image/pro03.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 2
     }, {
       img: '/image/pro04.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 3
     }, {
       img: '/image/pro05.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 4
     }, {
       img: '/image/pro06.png',
       name: 'ST-E80',
-      price: '3590'
+      price: '3590',
+      id: 5
     }]
   },
+  todetail(e) {
+    console.log(e);
+    let id = e.currentTarget.dataset.id
+    console.log(id);
+    wx.navigateTo({
+      url: '../detail/detail?id=' + id,
+      success: function (res) {
+        // success
+        console.log("'../detail/detail?id='  "+id);
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
 
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
